6307da0b5cf46bab8ab30df2034e1a8155d560f6
ResaloliPT <roberto_pires97@sapo.pt>
2016-07-19 01:37:52 +0000

    Add files via upload

---
+++ Bluetooth_Final/Bluetooth_Final.ino
@@ -0,0 +1,193 @@
+#define bluetooth Serial3
+#define debug Serial
+
+String al1Time = "11:11";
+String al2Time = "11:11";
+bool al1ck = true;
+bool al2ck = true;
+double al1KG = 0.0;
+double al2KG = 0.0;
+bool alerted = false;
+int nDosesAl1 = 3;
+int nDosesAl2 = 3;
+
+void setup() {
+  debug.begin(9600);
+  startBluetooth();
+}
+
+void loop() {
+  String btSerial = "";
+  while (bluetooth.available()) {
+    delay(50);
+    char rxData = (int)bluetooth.read();
+    btSerial = btSerial + (String)rxData;
+  }
+  if (btSerial.length() > 3) {
+    debug.println("BT: " + btSerial);
+    handleBT(btSerial);
+  }
+}
+
+void startBluetooth() {
+  debug.println("Iniciando comunicação Bluetooth!");
+  bluetooth.begin(115200);
+  bluetooth.print("$$$");
+  delay(50);
+  bluetooth.println("SU,9600");
+  delay(50);
+  bluetooth.begin(9600);
+  bluetooth.println("---");
+  delay(50);
+  debug.println("Bluetooth Pronto!");
+}
+
+void handleBT(String buff) {
+  String str = buff;
+  while(str.length() > 0){
+    if(str.substring(0, 4).indexOf("al1/") > -1){
+      if (str.substring(0, 6).indexOf("al1/1d") > -1) {
+        str = str.substring(6);
+        debug.println("Alimentador 1 dar 1 Dose!");
+      }
+      if (str.substring(0, 6).indexOf("al1/ck") > -1) {
+        if(str.substring(0, 9).indexOf("al1/ck/s/") > -1){
+          debug.println("Defenir valor check do Alimentador 1: " + str.substring(9, 10));
+          al1ck = (bool)str.substring(9, 10).toInt();
+          str = str.substring(10);
+        }
+      }
+      if (str.substring(0, 6).indexOf("al1/ti") > -1) {
+        if(str.substring(0, 9).indexOf("al1/ti/s/") > -1){
+          int limite = 10+numOfDidits(str.substring(9, 13));
+          if(str.substring(9, 13).indexOf("al1") >=0 || str.substring(9, 13).indexOf("al2") >=0 ){
+            debug.println(str.substring(9, 13));
+            limite--;
+          }
+          debug.println("Defenir valor tempo do Alimentador 1: " + str.substring(9, limite));
+          al1Time = str.substring(9, limite);
+          str = str.substring(limite);
+        }
+      }
+      if (str.substring(0, 6).indexOf("al1/gm") > -1) {
+        if(str.substring(0, 9).indexOf("al1/gm/s/") > -1){
+          int limite = 10+numOfDidits(str.substring(9, 13));
+          if(str.substring(9, 13).indexOf("al1") >=0 || str.substring(9, 13).indexOf("al2") >=0 ){
+            debug.println(str.substring(9, 13));
+            limite--;
+          }
+          debug.println("Defenir valor peso do Alimentador 1: " + str.substring(9, limite));
+          al1KG = str.substring(9, limite).toFloat();
+          str = str.substring(limite);
+        }
+      }
+    }else if(str.substring(0, 4).indexOf("al2/") > -1){
+      if (str.substring(0, 6).indexOf("al2/1d") > -1) {
+        str = str.substring(6);
+        debug.println("Alimentador 2 dar 1 Dose!");
+      }
+      if (str.substring(0, 6).indexOf("al2/ck") > -1) {
+        if(str.substring(0, 9).indexOf("al2/ck/s/") > -1){
+          debug.println("Defenir valor check do Alimentador 2: " + str.substring(9, 10));
+          al2ck = (bool)str.substring(9, 10).toInt();
+          str = str.substring(10);
+        }
+      }
+      if (str.substring(0, 6).indexOf("al2/ti") > -1) {
+        if(str.substring(0, 9).indexOf("al2/ti/s/") > -1){
+          int limite = 10+numOfDidits(str.substring(9, 13));
+          if(str.substring(9, 13).indexOf("al1") >=0 || str.substring(9, 13).indexOf("al2") >=0 ){
+            debug.println(str.substring(9, 13));
+            limite--;
+          }
+          debug.println("Defenir valor tempo do Alimentador 2: " + str.substring(9, limite));
+          al2Time = str.substring(9, limite);
+          str = str.substring(limite);
+        }
+      }
+      if (str.substring(0, 6).indexOf("al2/gm") > -1) {
+        if(str.substring(0, 9).indexOf("al2/gm/s/") > -1){
+          int limite = 10+numOfDidits(str.substring(9, 13));
+          if(str.substring(9, 13).indexOf("al1") >=0 || str.substring(9, 13).indexOf("al2") >=0 ){
+            debug.println(str.substring(9, 13));
+            limite--;
+          }
+          debug.println("Defenir valor peso do Alimentador 2: " + str.substring(9, limite));
+          al2KG = str.substring(9, limite).toFloat();
+          str = str.substring(limite);
+        }
+      }
+    }else if (str.substring(0, 5).indexOf("al/al") > -1) {
+        alerted = false;
+        nDosesAl1 = 3;
+        nDosesAl2 = 3;
+        str = str.substring(5);
+      }else{
+      str = "";
+    }
+  }
+  bluetooth.flush();
+}
+
+int numOfDidits(String str){
+  int num = 0;
+  for(int i = 0; i < str.length();i++){
+    if((char)str[i] == '0'||'1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'){
+      num++;
+    }
+    delay(10);
+  }
+  return num;
+}
+
+/*
+void handleBT(String str) {
+  while (str.length() > 0) {
+    if (str.indexOf("al1/1d") > 0) {
+      str.replace("al1/1d", "");
+      debug.println("Alimentador 1 dar 1 Dose!");
+      //dosar(1);
+    }
+    if (str.indexOf("al2/1d") > 0) {
+      str.replace("al2/1d", "");
+      debug.println("Alimentador 2 dar 1 Dose!");
+      //dosar(1);
+    }
+    if (str.indexOf("al1t/") > 0) {
+      al1Time = str.substring(str.indexOf("al1t/") + 5 , str.indexOf("al1t/") + 11);
+      str.replace(str.substring(str.indexOf("al1t/"), str.indexOf("al1t/") + 11), "");
+      debug.println("Defenido tempo do Alimentador 1 para: " + (String)al1Time);
+    }
+    if (str.indexOf("al2t/") > 0) {
+      al2Time = str.substring(str.indexOf("al2t/") + 5 , str.indexOf("al2t/") + 11);
+      str.replace(str.substring(str.indexOf("al2t/"), str.indexOf("al2t/") + 11), "");
+      debug.println("Defenido tempo do Alimentador 2 para: " + (String)al2Time);
+    }
+    if (str.indexOf("al1ck/get") > 0) {
+      bluetooth.println(al1ck);
+      debug.println("Enviar check do Alimentador 1 para Bluetooth");
+      str.replace(str.substring(str.indexOf("al1t/get"), str.indexOf("al1t/get") + 9), "");
+    } else {
+      if ((bool)str.substring(str.indexOf("al1ck/") + 6 , str.indexOf("al1t/get") + 11) == true) {
+        al1ck = true;
+        str.replace(str.substring(str.indexOf("al1t/"), str.indexOf("al1t/get") + 10), "");
+      } else {
+        al1ck = false;
+        str.replace(str.substring(str.indexOf("al1t/"), str.indexOf("al1t/get") + 11), "");
+      }
+    }
+    if (str.indexOf("al2ck/get") > 0) {
+      debug.println("Enviar check do alimentador 2 para Bluetooth");
+      bluetooth.println(al2ck);
+      str.replace(str.substring(str.indexOf("al2t/get"), str.indexOf("al2t/get") + 9), "");
+    } else {
+      if ((bool)str.substring(str.indexOf("al2ck/") + 6 , str.indexOf("al2t/get") + 11) == true) {
+        al2ck = true;
+        str.replace(str.substring(str.indexOf("al2t/"), str.indexOf("al2t/get") + 10), "");
+      } else {
+        al2ck = false;
+        str.replace(str.substring(str.indexOf("al2t/"), str.indexOf("al2t/get") + 11), "");
+      }
+    }
+  }
+}*/
---
+++ GSM_Final/GSM_Final.ino
@@ -0,0 +1,89 @@
+
+#define SMS Serial2
+//SoftwareSerial SMS = SoftwareSerial(6,7);
+
+const int GSM_ON = 9;
+const int GSM_CHECK = A8;
+int data[1];
+
+void setup()
+{
+  Serial.begin(9600);
+  SMS.begin(19200);
+  delay(10);
+  pinMode(GSM_ON, OUTPUT);
+  turnGSMOn();
+  Serial.write("\nTest Start\n");
+  sendSMS();
+  //handleClock();
+}
+
+void sendSMS()
+{
+  SMS.print("AT+CMGF=1\r");
+  delay(100);
+  SMS.println("AT+CMGS=\"+351964292976\"");
+  delay(100);
+  SMS.println("Testing");
+  delay(100);
+  SMS.println((char)26);
+  delay(100);
+  SMS.println();
+  delay(5000);
+}
+
+void handleClock(){
+  /*
+   * data[0] = horas
+   * data[1] = minutos
+   */
+  SMS.flush();
+  delay(1000);
+  SMS.println("AT+CCLK?");
+  delay(1000);
+  String timeStr = "";
+  while(SMS.available()){
+    timeStr += (char)SMS.read();
+  }
+  timeStr = timeStr.substring(29, 34);
+  data[0] = timeStr.substring(0, 2).toInt();
+  data[1] = timeStr.substring(3).toInt();
+  Serial.println("Hora: " + (String)data[0] + ":" + (String)data[1]);
+}
+
+void turnGSMOn(){
+  Serial.println("Verificando se modulo de SMS está ligado!");
+  int nMedidas = 10;
+  int medidas[nMedidas];
+  for(int i = 0; i < nMedidas;i++){
+    medidas[i] = analogRead(GSM_CHECK);
+    delay(500);
+  }
+  int somaMedidas = 0;
+  for(int i = 0; i < nMedidas;i++){
+    somaMedidas += medidas[i];
+  }
+  int mediaMedidas = somaMedidas / nMedidas;
+  if(mediaMedidas>500){
+    Serial.println("Ligando modulo SMS!");
+    digitalWrite(GSM_ON, HIGH);
+    delay(3000);
+    digitalWrite(GSM_ON, LOW);
+    delay(5000);
+  }
+}
+
+void loop()
+{
+  while (SMS.available())
+  {
+    char rxData = (int)SMS.read();
+    Serial.print(String(rxData));
+  }
+
+  while (Serial.available())
+  {
+    char rxData1 = (int)Serial.read();
+    SMS.print(String(rxData1));
+  }
+}
